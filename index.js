02c6a3b0f651875fa383c26f3b782c40489557da
AndresECB <134890305+AndresECB@users.noreply.github.com>
2023-05-31 16:31:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@
       muñeco.src = "img/muñeco.png";
       tituloMensaje.textContent = "Ningún mensaje fue encontrado";
       parrafo.textContent = "Ingresa el texto que deseas encripta o desencriptar";
-      alert("Debes ingresar algún texto");
+      swal("Ooops!", "Debes ingresar algún texto", "warning");
     }
   }
 
@@ -46,33 +46,6 @@
         muñeco.src = "img/muñeco.png";
         tituloMensaje.textContent = "Ningún mensaje fue encontrado";
         parrafo.textContent = "Ingresa el texto que deseas encripta o desencriptar";
-        alert("Debes ingresar algún texto");
-      }
-  }
-
-
-  function desencriptar () {
-    let texto = document.getElementById("texto").value;
-    let tituloMensaje = document.getElementById("titulo-mensaje");
-    let parrafo = document.getElementById("parrafo");
-    let muñeco = document.getElementById("muñeco");
-
-    let textoCifrado = texto
-      .replace(/enter/gi, "e")
-      .replace(/imes/gi, "i")
-      .replace(/ai/gi, "a")
-      .replace(/ober/gi, "o")
-      .replace(/ufat/gi, "u");
-
-      if  (texto.length != 0) {
-          document.getElementById("texto").value = textoCifrado;
-          tituloMensaje.textContent = "Texto desencriptado con éxito";
-          parrafo.textContent = "";
-          muñeco.src = "img/desencriptado.jpg";
-      }else {
-        muñeco.src = "img/muñeco.png";
-        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
-        parrafo.textContent = "Ingresa el texto que deseas encripta o desencriptar";
-        alert("Debes ingresar algún texto");
+        swal("Ooops!", "Debes ingresar algún texto", "warning");
       }
   }
